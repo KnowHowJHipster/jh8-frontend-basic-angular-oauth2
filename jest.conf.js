8d85db2117ffccc55e8dd857692252c880fb005c
Dima Denysenko <coding@dimdnk.com>
2024-06-06 13:42:02 +0300

    feat: move app code to the src root

--- jest.conf.js
+++ jest.conf.js
@@ -22,7 +22,7 @@ module.exports = {
     ['jest-junit', { outputDirectory: '<rootDir>/build/test-results/', outputName: 'TESTS-results-jest.xml' }],
     ['jest-sonar', { outputDirectory: './build/test-results/jest', outputName: 'TESTS-results-sonar.xml' }],
   ],
-  testMatch: ['<rootDir>/src/main/webapp/app/**/@(*.)@(spec.ts)'],
+  testMatch: ['<rootDir>/src/app/**/@(*.)@(spec.ts)'],
   testEnvironmentOptions: {
     url: 'https://jhipster.tech',
   },
